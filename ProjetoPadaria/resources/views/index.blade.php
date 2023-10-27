e5ab426a9cef9896f742ad0135cc46db51062d4e
tbmelo01 <batistamelo2012@hotmail.com>
2023-10-26 22:18:12 -0300

    foi arrumado o menu e feitas algumas estilizações

--- ProjetoPadaria/resources/views/index.blade.php
+++ ProjetoPadaria/resources/views/index.blade.php
@@ -28,10 +28,12 @@
         <div class="menu">
             <nav>
                 <ul>
-                    <!-- <li><a href="produto-pag">Cadastrar Produtos</a></li> -->
-                    <li><a href="{{ route('grafico') }}">Login</a></li>
-                    <li><a href="#">Fale Conosco</a></li>
-                    <li><a href="#">Cadastre-se</a></li>
+                    <li><a href="#">Página Inicial</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Sobre Nós</a></li>
+                    <li><a href="#">Contato</a></li>
+                    <li><a href="#">Login</a></li>
+
                 </ul>
             </nav>
         </div>
@@ -42,15 +44,11 @@
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
             </ol>
+
             <div class="carousel-inner" style="height:40em">
-                <div
-                    class="carousel-item active"
-                    style="background-image: url('./img/carrossel/foto2.jpg');"></div>
-
+                <div class="carousel-item active"style="background-image: url('./img/carrossel/foto2.jpg');"></div>
                 <div class="carousel-item" style="background-image: url('./img/carrossel/foto1.jpg');"></div>
-
                 <div class="carousel-item" style="background-image: url('./img/carrossel/foto3.avif');"></div>
-
             </div>
 
             <a
@@ -70,56 +68,67 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
-  
         </div>
 
-        <br><br>
+        <br>
         <!-- </div> -->
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script
             src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
             integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-            crossorigin="anonymous"></script>
+            crossorigin="anonymous">
+        </script>
         <script
             src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
             integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-            crossorigin="anonymous"></script>
+            crossorigin="anonymous">
+        </script>
         <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
             integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-            crossorigin="anonymous"></script>
+            crossorigin="anonymous">
+        </script>
+
             <br>
-            <h1>Produtos</h1>
+                <h1>Produtos</h1>
             <br>
+
             <div id="overlay"></div>
-    <div class="container-produtos">
-    <?php// $count = 0; ?>
-        @foreach($produtos as $p)
-        <?php// if ($count % 4 == 0): ?>
-            <!-- <div class="product-row"> -->
-            <?php// endif; ?>
-        <div class="imagem-container">
-            <img src="img/fotos/{{$p->img}}" alt="Imagem {{$p->idProduto}}" class="imagem-produto-fora">
-            <div id="div-produto-{{$p->idProduto}}" class="div-produto">
-                <a href="/produto/escolhido/{{$p->idProduto}}"> <img src="img/fotos/{{$p->img}}" alt="Imagem {{$p->idProduto}}" class="imagem-produto-dentro"></a>
-                <br>
-                <div class="titulo"> {{$p->produto}}</div>
-                <br>
-                <div class="desc">{{$p->descProduto}}</div>
-                <br>
-                <div class="valor">Preço: {{$p->valorProduto}} R$</div>
-                <br>
-                <div class="datavalidade">Data de Validade: {{$p->dataValidade}}</div>
-                
-                <?php// if ($count % 4 == 3 || $loop->last): ?>
+
+        <div class="container-produtos">
+            
+    
+                 @foreach($produtos as $p)
+            
+                <div class="imagem-container">
+
+                    <img src="img/fotos/{{$p->img}}" alt="Imagem {{$p->idProduto}}" class="imagem-produto-fora">
+
+                    <div id="div-produto-{{$p->idProduto}}" class="div-produto">
+
+                        <a href="/produto/escolhido/{{$p->idProduto}}"> <img src="img/fotos/{{$p->img}}" alt="Imagem {{$p->idProduto}}" class="imagem-produto-dentro"></a>
+
+                        <br>
+
+                        <div class="titulo"> {{$p->produto}}</div>
+
+                        <br>
+
+                        <div class="desc">{{$p->descProduto}}</div>
+
+                        <br>
+
+                        <div class="valor">Preço: {{$p->valorProduto}} R$</div>
+
+                        <br>
+
+                        <div class="datavalidade">Data de Validade: {{$p->dataValidade}}</div>
                     </div>
-        <?php// endif; ?>
-            <!-- </div>   -->
+                        
+                </div>
+            @endforeach
         </div>
-        <?php// $count++; ?>
-        @endforeach
-    </div>
          
                 <script src="./js/script.js"></script>
     </body>
