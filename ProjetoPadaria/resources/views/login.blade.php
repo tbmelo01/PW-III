23c3dae64ad33340d2b1722f2028714c02fdf5e1
tbmelo01 <batistamelo2012@hotmail.com>
2023-11-29 20:37:53 -0300

    arrumei o login e fiz uma gambiarra no css

--- ProjetoPadaria/resources/views/login.blade.php
+++ ProjetoPadaria/resources/views/login.blade.php
@@ -6,12 +6,19 @@
     <title>Document</title>
     <link rel="stylesheet" href="./css/index.css">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
 </head>
 <body>
 
     <form class="formula" action="/login" method="post">
         {{ csrf_field() }}
+
+        <div>
+            <h1 class="log" ><b>Login</b></h1>
+        </div>
+
+            <br><br><br>
+
         <div>
             <input type="text" name="email" />            
         </div>
---
+++ ProjetoPadaria/resources/views/login2.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container" >   
+        <form class="formula" action="/login" method="post">
+            {{ csrf_field() }}
+            
+            
+
+            <h1> Loguin</h1>   
+            
+            <br><br><br>
+            <div>
+                <input type="text" name="email" />            
+            </div>
+
+            <div>
+                <input type="text" name="password"/>            
+            </div>
+
+            <div>
+                <input type="submit" value="Login" />
+            </div>  
+        </form>
+    </div>
+</body>
+</html>
